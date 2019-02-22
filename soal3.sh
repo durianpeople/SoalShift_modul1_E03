34361fb9721c53b24c4076d228f7f497e1171da7
AkmalFakhriB <akmalbaihaqi30@gmail.com>
2019-02-22 17:42:35 +0700

    First commit

---
+++ soal3.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+choose() { echo ${1:RANDOM%${#1}:1} $RANDOM; }
+pass="$({   choose '0123456789'
+  choose 'abcdefghijklmnopqrstuvwxyz'
+  choose 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  for i in $( seq 1 $(( 9 )) )
+     do
+        choose '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+     done
+ } | sort -R | awk '{printf "%s",$1}')"
+
+flag=1
+while true
+do
+if [ ! -f /home/akmal/Documents/Password$flag.txt ]
+then
+	echo $pass > /home/akmal/Documents/Password$flag.txt
+	break
+	#flag=$((flag + 1))
+else
+	#echo $pass > /home/akmal/Documents/Password$flag.txt
+        #break	
+	flag=$((flag + 1))
+fi
+done
---
+++ soal5alt.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cat /var/log/syslog | awk '$0 !~ /sudo/ {print}' | awk 'IGNORECASE=1; /cron/ {print}' | awk 'NF < 12' >> /home/akmal/modul1
+
+#crontab 2-30/6 * * * * /home/akmal/Documents/Praktikum1_5.sh
